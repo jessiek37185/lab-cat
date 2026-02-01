8a4954a5e177e7b771b0161c8edb8f5e97e568f6
jessiek37185 <KKong76@students.claremontmckenna.edu>
2026-02-01 01:30:17 -0800

    Fix cat.py

--- cat.py
+++ cat.py
@@ -4,17 +4,16 @@ This program prints stdin to the screen.
 import sys
 
 def cat(file):
-    while True:
-        chunk = file.read(4096)
-        if not chunk:
-            break
-        sys.stdout.buffer.write(chunk)
-
+    buf = bytearray(4096)
+    mv = memoryview(buf)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         for filename in sys.argv[1:]:
-            with open(filename, "rb") as f:
-                cat(f)
+            if filename == "-":
+                cat(sys.stdin.buffer)
+            else:
+                with open(filename, "rb") as f:
+                    cat(f)
     else:
         cat(sys.stdin.buffer)
